98ef1dea3e853319f238f4fb8cc9eef44a5f301e
Ivan <elreizerwtf@gmail.com>
2025-08-08 15:44:10 -0300

    testing changes

--- licitacionesPC.cjs
+++ licitacionesPC.cjs
@@ -15,12 +15,13 @@ const urls = [
   "https://www.panamacompra.gob.pa/Inicio/#/oportunidades-de-negocio/servicios-publicos-y-servicios-relacionados-con-el-sector-publico?q=0nIvNWasJmuDDFIy9GdjV2UgwWZg42bjBycvRWYu9WajFGblJFIz9WajlmdyV2UgkHIz92YpxmY6OMUgM3bpNWa2JXZTJiOiUGb0lGdiwyM4ojIvJnY1JFZJJye",
   "https://www.panamacompra.gob.pa/Inicio/#/oportunidades-de-negocio/telecomunicaciones-y-radiodifusion-de-tecnologia-de-la-informacion?q=Qfi42sDn2Yh1mcvZmbpBSYsBSZkBSYtO8Zvx2buNWZ0BSZkBibzOcazVnZpR2bpRWYyBSegMXZu9WajF2Yp5Wdt92YlxWZUJiOiUGb0lGdiwyM0ojIvJnY1JFZJJye"
 ];
-
+  async (res) => {
   const browser = await puppeteer.launch({ 
       headless: true,
       executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
       args: ['--no-sandbox', '--disable-setuid-sandbox','--single-process','no-zugote']
     });
+    }
 
 app.get('/pc', async (req, res) => {
   try {
